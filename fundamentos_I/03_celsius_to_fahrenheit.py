1732e0d179586903c546c693a8f72da5fe049efb
Javier Martínez <jmartinez@develsystems.com>
2023-02-21 21:50:17 -0600

    [FIX] Shorted solution and design file

--- fundamentos_I/03_celsius_to_fahrenheit.py
+++ fundamentos_I/03_celsius_to_fahrenheit.py
@@ -11,7 +11,7 @@ while True:
 fahrenheit_degrees = celsius_degrees * 1.8 + 32
 
 print(f"{celsius_degrees} grados Celsius equivale a {fahrenheit_degrees} grados Fahrenheit.")
-print("Link del análisis y diseño de la solución: https://universidaddavincid-my.sharepoint.com/:b:/g/personal/202302745_estudiante_udv_edu_gt/ESNC8Jwru_1Ml51eKeEUrH4BgRiEfqf5Slp2Ov-J0FOk7A?e=amJ6Hd")
+print("\nLink del análisis y diseño de la solución: shorturl.at/uQVW5")
 input('Hecho por Jimmy Martínez - Carné No. 202302745')
 
 ## Jimmy Martínez - Carné No. 202302745
