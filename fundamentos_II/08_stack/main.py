9d97d58ba8370f8482e2fd3dc3049c74196126e2
Jimmy Martínez - SE II <jmartinez@develsystems.com>
2023-12-04 21:15:17 -0600

    [UPDATE] - Manage negative numbers exception - Repo url was fixed

--- fundamentos_II/08_stack/main.py
+++ fundamentos_II/08_stack/main.py
@@ -1,4 +1,5 @@
 from pila import Pila
+from validation import NoPositiveNumberException
 
 pila = Pila()
 
@@ -7,8 +8,12 @@ for i in range(5):
     while True:
         try:
             # APILAMOS SI ES UN ENTERO VÁLIDO
-            elemento = int(input(f"Ingrese el a apilar: "))
-            pila.apilar(elemento)
+            elemento = int(input(f"Ingrese el valor a apilar: "))
+            
+            if elemento < 0:
+                raise NoPositiveNumberException("Por favor, ingrese un número entero positivo.")
+            else:
+                pila.apilar(elemento)
 
             # DESAPILAMOS CUANDO LA CANTIDAD DE ELEMENTOS SEA MAYOR A TRES
             if len(pila.elementos) > 3:
@@ -16,9 +21,11 @@ for i in range(5):
             break
         except ValueError:
             print("[ERROR] Solamente están permitidos los valores enteros.")
+        except NoPositiveNumberException as e:
+            print(f"[ERROR] {e}")
 
-print("Elementos restantes en la pila:", pila.elementos)
+print("\nElementos restantes en la pila:", pila.elementos)
 input("Presione enter para cerrar el programa...")
 
 ## Jimmy Martínez - Carné No. 202302745
-## Repo - https://github.com/MegaGone/udv/blob/develop/fundamentos_II/08_final/
+## Repo - https://github.com/MegaGone/udv/blob/develop/fundamentos_II/08_stack/
